e9c3c8cc4137f09f3ef81e0549ffd7b9eae59580
kamilla-abdullayeva <abdullayeva-kamilla@inbox.ru>
2024-09-09 12:55:45 +0500

    Add files via upload

---
+++ example/src/test/java/com/kamilla/ExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kamilla;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
